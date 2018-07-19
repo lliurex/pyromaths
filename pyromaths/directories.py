c119e2f4e5ddb9e288d8b2a50a462856541e4abc
Louis <spalax@gresille.org>
2018-07-19 19:17:20 +0200

    Correction de bug si la variable d'environnement HOME n'est pas définie

--- pyromaths/directories.py
+++ pyromaths/directories.py
@@ -8,14 +8,12 @@ import pkg_resources
 DATADIR = pkg_resources.resource_filename("pyromaths", "data")
 EXODIR = os.path.join(DATADIR, "exercices")
 LOCALEDIR = os.path.join(DATADIR, "locale")
+HOME = os.path.expanduser("~")
 
 if os.name == 'nt': # Windows
-    HOME = os.environ['USERPROFILE']
     CONFIGDIR = os.path.join(os.environ['APPDATA'], "pyromaths")
 elif sys.platform == "darwin":  # Mac OS X
-    HOME = os.environ['HOME']
     CONFIGDIR = os.path.join(HOME, "Library", "Application Support", "Pyromaths")
 else: # Linux (et autres ?)
-    HOME = os.environ['HOME']
     CONFIGDIR = os.path.join(HOME, ".config", "pyromaths")
 
